192f097cb12981efc30c8baf10476779c2f9858d
matteo <matteo@MacBook-Air-de-matteo.local>
2025-07-21 16:02:05 +0200

    enlever commentaire

--- script.js
+++ script.js
@@ -1,21 +1,3 @@
-// async function naruto(){
-//     let i = 0
-//     let response = await fetch(`https://dattebayo-api.onrender.com/characters?page=${i}`) 
-//         let data = await response.json()
-// console.log(data)
-//     for( i = 0 ; i < data.characters.length ; i++ )
-//     {
-//        response = await fetch(`https://dattebayo-api.onrender.com/characters?page=${i}`) 
-    
-//    // if(data.characters[i].name === "Naruto Uzumaki")
-    
-//         console.log(data.characters[i].name)
-//         console.log(i)
-    
-//     }
-// }   
-// naruto()
-
 async function naruto() {
     const totalPages = 72; // ou choisis une autre valeur
     for (let page = 1; page <= totalPages; page++) {
